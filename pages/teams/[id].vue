2c76a06e9776d0a9377046af5761df5cca4cfe95
Jokab <jakob@sigmastocks.com>
2024-04-10 21:10:21 +0200

    sort past quizes reverse

--- pages/teams/[id].vue
+++ pages/teams/[id].vue
@@ -23,7 +23,7 @@ const pastQuizes: PastQuizesRow[] = team.competitors.map((x) => {
     receivedPoints,
     placement: x.placement,
   };
-});
+}).sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
 export interface CategoryStatRecord {
   correctPercentage: string;
